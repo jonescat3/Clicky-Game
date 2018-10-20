afb82b8cafd2f19d55eb7dfa0c17091df36f7b44
Trina Raye <jonescatrina1@gmail.com>
2018-10-20 12:54:01 -0400

    updates

---
+++ clickygame/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>Ayanna Kosoko</p>
+	</header>
+);
+
+export default Footer;
--- flintstones/src/components/Wrapper/wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./wrapper.css";
-
-const wrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default wrapper;
